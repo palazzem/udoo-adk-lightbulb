5199d03da614c245e0d50b10add7a8a0209b4d09
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-01-19 18:05:44 +0100

    Added first AdkManager interfaces

---
+++ lightbulb/src/main/java/me/palazzetti/lightbulb/adk/IAdkManager.java
@@ -0,0 +1,18 @@
+package me.palazzetti.lightbulb.adk;
+
+import android.hardware.usb.UsbAccessory;
+
+/**
+ * Defines ADK interfaces
+ */
+
+public interface IAdkManager {
+    void openAccessory(UsbAccessory usbAccessory);
+    void closeAccessory();
+    void readText();
+    void sendText(String text);
+
+    // Activity related interfaces
+    void pauseAdk();
+    void resumeAdk();
+}
